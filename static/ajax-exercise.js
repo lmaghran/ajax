38bf8e8b58cfd38855ce5544dfca2e4841ffc990
Your Name <you@example.com>
2020-02-07 13:09:58 -0800

    almost finished- trouble with the order melons fxn

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -43,8 +43,24 @@ function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
+    const melonOrder = {
+      'qty':Number($("qty-field").val()),
+      'type':$("melon-type-field").val()
+    }
+
+    $.post('/order-melons.json', melonOrder, (res) => {
+        if (res.code === 'ERROR'){
+          $('#order-status').addClass('order-error');}
+
+     $('#order-status').html(res.msg);
+
+
+        })
+
+    }
+
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+
 
 $("#order-form").on('submit', orderMelons);
 
